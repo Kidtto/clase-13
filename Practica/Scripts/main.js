c8b4665ff0ced3f56ae211a8a3d6976bd8a859bd
Kidtto <109235894+Kidtto@users.noreply.github.com>
2022-09-06 17:42:37 -0500

    Add files via upload

---
+++ Practica/Scripts/main.js
@@ -0,0 +1,24 @@
+'use strict'
+import{ validateString, validateAvarage } from"./valedate.js";
+const btnAgregar = document.querySelector("#btn");
+const btnMostra = document.querySelector("#btnMostrar");
+
+// propiedad y con .addEventeListener
+btnAgregar.addEventListener("click",() => {
+    const nombre = document.querySelector("#nombre").value;
+    const apellido = document.querySelector("#apellido").value;
+    const promedio = document.querySelector("#promedio").value;
+    const opcion = document.querySelector("#opcion").value;
+     
+
+    if(validateString(nombre)&&validateString(apellido) && opcion !="0"){
+        if (isNaN(parseFloat(promedio)) && (validateAvarage(promedio))){
+            console.log("validado");
+ 
+        }else{
+            console.log("Error promedio");
+        }
+    }else{
+        console.log("Error de datos");
+    }
+}); 
